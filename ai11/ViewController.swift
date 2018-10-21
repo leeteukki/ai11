e6d31fe2b07cd67c34fbe703957a50da673b3731
= <=>
2018-10-21 17:18:44 +0900

    Initial Commit

---
+++ ai11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ai11
+//
+//  Created by lee chat on 21/10/2018.
+//  Copyright © 2018 lee chat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ai11Tests/ai11Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ai11Tests.swift
+//  ai11Tests
+//
+//  Created by lee chat on 21/10/2018.
+//  Copyright © 2018 lee chat. All rights reserved.
+//
+
+import XCTest
+@testable import ai11
+
+class ai11Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
