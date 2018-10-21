d6fb503fdcbee77bc4fd6c40552549883e697099
leeteukki <tklohj@naver.com>
2018-10-21 17:55:32 +0900

    tableView

---
+++ ai11/MainViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainViewController.swift
+//  ai11
+//
+//  Created by lee chat on 21/10/2018.
+//  Copyright © 2018 lee chat. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
