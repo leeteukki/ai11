5601cc2b33c11e2aaf9d96563b1d4face9b2ebf5
leeteukki <tklohj@naver.com>
2018-10-21 17:31:56 +0900

    First ViewController

---
+++ MainViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MainViewController.swift
+//  ai11
+//
+//  Created by lee chat on 21/10/2018.
+//  Copyright © 2018 lee chat. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
